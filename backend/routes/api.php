ef93ff33fac79ce09e7a6fbe0c2eb7613acb782a
berKrz <bkrzyuy@gmail.com>
2026-03-06 14:10:10 -0300

    fix: rate limit backend routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\UrlController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/shorten', [UrlController::class, 'store']);
-Route::get('/{shortUrl}', [UrlController::class, 'redirect']);
+Route::post('/shorten', [UrlController::class, 'store'])->middleware('throttle:20,1');
+Route::get('/{shortUrl}', [UrlController::class, 'redirect'])->middleware('throttle:120,1');
